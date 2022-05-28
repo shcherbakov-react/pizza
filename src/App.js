da7a32f17a2c39107d2b9f43a09edb0c3bbc10cf
shcherbakov-react <shcherbakov-dev@yandex.ru>
2022-05-28 23:09:31 +0300

    fix branch main because 2nd branch was crashed

--- src/App.js
+++ src/App.js
@@ -1,42 +1,19 @@
 import './App.css';
 import './scss/app.scss';
 import Header from './components/Header/Header';
-import Categories from './components/Categories/Categories';
-import Sort from './components/Sort/Sort';
-import PizzaBlock from './components/PizzaBlock/PizzaBlock';
-import Skeleton from './components/PizzaBlock/Skeleton';
+import Home from '../src/pages/Home';
+// import NotFound from '../src/pages/NotFound';
 
 // import pizzas from './assets/pizza.json';
 import React from 'react';
 
 function App() {
-  const [items, setItems] = React.useState([]);
-  const [isLoading, setIsLoading] = React.useState(true);
-
-  React.useEffect(() => {
-    fetch('https://628905bd7af826e39e67d760.mockapi.io/items')
-      .then((res) => res.json())
-      .then((arr) => {
-        setItems(arr);
-        setIsLoading(false);
-      });
-  }, []);
-
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {isLoading
-              ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-              : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
-          </div>
+          <Home />
         </div>
       </div>
     </div>
