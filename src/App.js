955f52b2c0919f1ff3b051662d214c8227222705
shcherbakov-react <shcherbakov-dev@yandex.ru>
2022-05-30 23:45:36 +0300

    css modules + need fix route

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from '../src/pages/Home';
 
 // import pizzas from './assets/pizza.json';
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
       <Header />
       <div className="content">
         <div className="container">
-          <Home />
+          <Routes></Routes>
         </div>
       </div>
     </div>
